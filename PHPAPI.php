89a791bc19ec7c4ac0e879d71f2a411e8ca66db1
James Swift <swiftscripts@gmail.com>
2014-03-20 21:11:36 +0000

    Imporved methods.

--- PHPAPI.php
+++ PHPAPI.php
@@ -7,11 +7,11 @@ class Exception extends \Exception {
 }
 
 class PHPAPI {
-	private $config=array();
-	private $views=array();
+	private $config;
+	private $views;
 	private $allowedMethods=array("GET","POST","PUT","DELETE");
 	
-	public function __construct($config){
+	public function __construct($config=null){
 		$this->loadConfig($config);
 	}
 	
@@ -21,58 +21,79 @@ class PHPAPI {
 			$this->config = $config;
 			return true;
 		//If config is a path to a json file, parse and load it
-		} else {
-			return $this->loadConfigFromJSON($config);
+		} else if ($config!==null){
+			return $this->loadJSON($config);
 		}	
 	}
 	
-	public function loadConfigFromJSON($config){
+	public function loadJSON($config){
 		//Check JSON file exists and has right extension
 		if (is_file($config) && strtolower(substr($config, -5))===".json"){
 			//Decode JSON and parse it
-			$this->config=json_decode(file_get_contents($config), true);
+			$file=json_decode(file_get_contents($config), true);
+			$this->registerViews($file['views']);
+			$this->config=$file['config'];
 			return true;
 		}
 		return false;
 	}
 	
 	public function request($method, $URI, $headers = null, $body = null){
-		
+		//Try to find the right view
+		$view=$this->findViewByURI($URI);
+		var_dump($view);
+	}
+	
+	public function findViewByURI($URI){
+		foreach ($this->views as $view){
+			if (preg_match($view['pattern'], $URI)==true){
+				return $view;
+			}
+		}
+		return null;
+	}
+	
+	public function getViews(){
+		return $this->views;
+	}
+	
+	public function set($what, $to){
+		$this->config[$what]=$to;
 	}
 	
 	public function registerView($view){
 		
 		//Check Methods array
 		if (!isset($view['allowedMethods']) || !is_array($view['allowedMethods']) || sizeof($view['allowedMethods'])<1 ) {
-			throw new Exception("The array passed to registerView must contain an non-empty array named 'allowedMethods'.","BadMethodsArrayDefinition");
+			throw new Exception("The array passed to registerView must contain an non-empty array named 'allowedMethods'.");
 		}
 		foreach($view['allowedMethods'] as &$method){
 			$method=strtoupper($method);
 			if (in_array($method, $this->allowedMethods)!==true){
-				throw new Exception("Unknown method specified in the 'allowedMethods' array.","BadMethodDefinition");
+				throw new Exception("Unknown method specified in the 'allowedMethods' array.");
 			}
 		}
 		
 		//Check request is string
-		if (!is_string($view['request']) || strlen($view['request'])<1) {
-			throw new Exception("The array passed to registerView must contain an non-empty string named 'request'.","BadRequestDefinition");
+		if (!is_string($view['pattern']) || strlen($view['pattern'])<1) {
+			throw new Exception("The array passed to registerView must contain an non-empty string named 'pattern'.");
 		}
 		
 		//Check call is string
 		if (!is_string($view['call']) || strlen($view['call'])<1) {
-			throw new Exception("The array passed to registerView must contain an non-empty string named 'call'.","BadCallDefinition");
+			throw new Exception("The array passed to registerView must contain an non-empty string named 'call'.");
 		}
 		
 		//Check require exists
 		if (isset($view['require'])){
-			if (!is_string($view['require']) || !is_file($view['require'])) {
-				throw new Exception("The 'require' path you specified doesn't exist.","BadRequireDefinition");
+			if (!is_string($view['require']) || !is_file($this->config['requireRoot'].$view['require'])) {
+				throw new Exception("The 'require' path you specified (\"".$this->config['requireRoot'].$view['require']."\") doesn't exist.");
 			}
 		}
 		
 		$this->views[]=array(
 			"allowedMethods"=>$view['allowedMethods'],
-			"request"=>$view['request'],
+			"pattern"=>$view['pattern'],
 			"call"=>$view['call'],
 			"require"=>$view['require']
 		);
@@ -80,12 +101,11 @@ class PHPAPI {
 	
 	public function registerViews($array){
 		if (!is_array($array) || sizeof($array)<1){
-			throw new Exception("Method registerView requires a non-empty array.", "InvalidVariableType"); 
+			throw new Exception("Method registerView requires a non-empty array."); 
 		}
 		foreach($array as $view){
 			$this->registerView($view);
-		}
-		
+		}		
 	}
 }
 

310b5e2b72383f711f0b69a6aae3ee22fbc2248e
James Swift <swiftscripts@gmail.com>
2017-03-22 21:19:11 +0000

    Updated default database.

--- db.sql
+++ db.sql
@@ -1,7 +1,16 @@
 CREATE DATABASE IF NOT EXISTS `swdapi` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `swdapi`;
 
-CREATE TABLE IF NOT EXISTS `nonce` (
+CREATE TABLE `clients` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `secret` char(64) NOT NULL,
+  `name` varchar(140) NOT NULL,
+  `last_active` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
+
+CREATE TABLE `nonce` (
   `value` varchar(10) CHARACTER SET ascii COLLATE ascii_bin NOT NULL,
   `expires` int(11) NOT NULL,
   UNIQUE KEY `value` (`value`)
